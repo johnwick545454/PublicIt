5c147b1799a79d4a88302b556278f697b6a67809
blessed-me <42597894+blessed-me@users.noreply.github.com>
2018-08-22 10:47:27 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,26 @@
+package Answer1;
+
+public class Employee implements Addressable {
+	String state;
+	String city;
+
+	public Employee(String state, String city) {
+		super();
+		this.state = state;
+		this.city = city;
+	}
+
+	
+	@Override
+	public String getState() {
+		// TODO Auto-generated method stub
+		return state;
+	}
+
+	@Override
+	public String getCity() {
+		// TODO Auto-generated method stub
+		return city;
+	}
+
+}
